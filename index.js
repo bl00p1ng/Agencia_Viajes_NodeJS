2053f139937960c346a6ae4d2eebb79301ae5801
bl00p1ng <blooping@protonmail.com>
2022-07-10 19:09:17 -0500

    370 Habilitando la Sintaxis de Imports y Exports

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
-const express = require('express')
+import express from 'express'
 
 const app = express()
 
 // Definir puerto
 const port = process.env.PORT || 4000
+
+app.get('/', (req, res) => {
+    res.send('Inicio')
+})
 
 // Arrancar el servidor
 app.listen(port, () => {
